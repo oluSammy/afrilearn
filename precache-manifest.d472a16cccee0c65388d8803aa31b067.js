333912b391ff1bc2ea4925cdaaf88e34c7fc7d6a
oluSammy <olumorinsammy@gmail.com>
2020-10-01 04:31:09 -0700

    Updates

---
+++ precache-manifest.d472a16cccee0c65388d8803aa31b067.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8ff0a0c7398cb0f9b72d695e0729df",
+    "url": "/afrilearn/index.html"
+  },
+  {
+    "revision": "be9c65e713e69203efe5",
+    "url": "/afrilearn/static/css/main.32e3d856.chunk.css"
+  },
+  {
+    "revision": "9b887f83fa424609ed7c",
+    "url": "/afrilearn/static/js/2.e82ba67e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/afrilearn/static/js/2.e82ba67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9c65e713e69203efe5",
+    "url": "/afrilearn/static/js/main.bf08cde5.chunk.js"
+  },
+  {
+    "revision": "7aaa745bff362f7003b2",
+    "url": "/afrilearn/static/js/runtime-main.84ac36d4.js"
+  },
+  {
+    "revision": "5c2db8b1013319f2eca1d4b39bab911a",
+    "url": "/afrilearn/static/media/afrilearn_logo.5c2db8b1.png"
+  },
+  {
+    "revision": "7146862611465d56e1fbf72a6bfc5347",
+    "url": "/afrilearn/static/media/banner.71468626.jpg"
+  },
+  {
+    "revision": "f7d8bfcc27f3769ff3595d02cb577079",
+    "url": "/afrilearn/static/media/jamb.f7d8bfcc.png"
+  },
+  {
+    "revision": "7156e57a7117f322af818badbbf1c30c",
+    "url": "/afrilearn/static/media/necco.7156e57a.png"
+  },
+  {
+    "revision": "1e5ea518198e8ec38f1341f164932e0a",
+    "url": "/afrilearn/static/media/professional.1e5ea518.webp"
+  },
+  {
+    "revision": "1f3aff80b2007ccb2af64fe4a2461d15",
+    "url": "/afrilearn/static/media/waec.1f3aff80.png"
+  }
+]);
